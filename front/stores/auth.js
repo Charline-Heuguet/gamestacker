5ed6364b2119c96e286efbe91837bc990be86c96
MaxenceCodeFR <maxence.hattabi@gmail.com>
2024-10-17 15:45:54 +0200

    signup, login et logout controller

---
+++ front/stores/auth.js
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia';
+import { useStorage } from '@vueuse/core';
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        token: useStorage('token', null),
+    }),
+    actions: {
+        async register(email, password) {
+            try {
+                await $fetch('/api/register', {
+                    method: 'POST',
+                    body: { email, password }
+                });
+                console.log('User registered successfully');
+            } catch (error) {
+                console.error('Erreur lors de l\'inscription:', error);
+                throw error;
+            }
+        },
+        async login(email, password) {
+            try {
+                const response = await $fetch('/api/login', {
+                    method: 'POST',
+                    body: { email, password }
+                });
+                this.token = response.token;
+            } catch (error) {
+                console.error('Erreur de login:', error);
+                throw error;
+            }
+        },
+        logout() {
+            this.token = null;
+        },
+    },
+    getters: {
+        isAuthenticated: (state) => !!state.token,
+    },
+});
