4326622cb77bc1bdc7f0486c847d3702f0c234b5
charline <charline.heuguet@gmail.com>
2024-11-07 16:13:18 +0100

    mentions legales

---
+++ front/pages/mentions-legales.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <h1>Mentions légales</h1>
+        <h2>Informations légales :</h2>
+        <p>Nom du Site : GAME STACKERS </p>
+        <p>Adresse : 2 Esplanade de la Gare, 33110 Le Bouscat</p>
+        <p>Adresse mail de contact: gamestackers@gmail.com</p>
+        <p>Directeur de publication : Vincent COMPARATO</p>
+        <p>Hébergeur : PLANET HOSTER 4416 Louis-B. -Mayer, Laval, Québec, Canada H7P 0G1 (numéro de téléphone :  01 76 60 41 43)</p>
+
+        <h2>Propriété intellectuelle :</h2>
+        <p>Le site GAME STACKERS et chacun des éléments qui le composent (tels que les marques, logos, photographies,
+            textes, illustrations, images, vidéos, etc.) sont protégés par la législation en vigueur sur la propriété
+            intellectuelle. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie
+            des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite
+            préalable de GAME STACKERS.
+        </p>
+
+        <h2>Données personnelles</h2>
+        <p>Les données personnelles collectées sur le site sont uniquement destinées à GAME STACKERS. Aucune donnée
+            personnelle n’est cédée à des tiers. Les données collectées sont conservées pendant une durée de 3 ans.
+            Conformément à la loi « informatique et libertés », vous pouvez exercer votre droit d’accès aux données
+            vous concernant et les faire rectifier en contactant GAME STACKERS à l’adresse suivante :
+            burdigalart@gmail.com </p>
+
+        <h2>Responsabilité</h2>
+        <p>GAME STACKERS s’efforce d’assurer au mieux de ses possibilités, l’exactitude et la mise à jour des
+            informations diffusées sur ce site. Toutefois, GAME STACKERS décline toute responsabilité pour toute
+            imprécision, inexactitude ou omission portant sur des informations disponibles sur le site. Les utilisateurs
+            sont invités à vérifier les informations par d'autres moyens, y compris en contactant directement
+            GAME STACKERS.</p>
+
+        <h2>Liens hypertextes</h2>
+        <p>Le site GAME STACKERS peut contenir des liens hypertextes renvoyant à d’autres sites internet. GAME STACKERS ne
+            saurait être tenu pour responsable du contenu de ces sites et des dommages pouvant résulter de l’utilisation
+            de ces sites. Les utilisateurs du site GAME STACKERS ne peuvent en aucun cas mettre en place un lien
+            hypertexte en direction de ce site sans l’autorisation préalable et écrite de GAME STACKERS.</p>
+
+        <h2>Modification des mentions légales</h2>
+        <p>GAME STACKERS se réserve le droit de modifier les présentes mentions à tout moment. L’utilisateur s’engage
+            donc à les consulter régulièrement.</p>
+
+        <h2>Droit applicable et juridiction compétente</h2>
+        <p>Les présentes mentions légales sont régies par le droit français. En cas de litige et à défaut d’accord
+            amiable, le différend sera porté devant les tribunaux compétents de Bordeaux.</p>
+
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    p {
+        margin-bottom: 30px;
+    }
+</style>
