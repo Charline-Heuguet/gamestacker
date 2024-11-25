8172872194be5dedf83b56ab430cbf04508d88d6
Vincent-Antoine <vincentcomparato69@gmail.com>
2024-11-25 15:37:27 +0100

    amélioration des fixtures, les images provennant des fixtures sont récupéré et assigné aléatoirement à un article ou a un compte user quand on lance les fixtures

--- back/src/DataFixtures/ArticleFixtures.php
+++ back/src/DataFixtures/ArticleFixtures.php
@@ -13,13 +13,22 @@ class ArticleFixtures extends Fixture
     {
         $faker = Factory::create();
 
+        // Liste des noms d'images disponibles
+        $imageNames = [
+            'article-image-1.jpg',
+            'article-image-2.jpg',
+            'article-image-3.jpg',
+            'article-image-4.jpg',
+        ];
+
         // Création de 10 articles
         for ($i = 1; $i <= 10; $i++) {
             $article = new Article();
             $article->setTitle($faker->sentence());
             $article->setContent($faker->paragraph());
             $article->setDate($faker->dateTimeThisYear());
-            $article->setImage($faker->imageUrl(640, 480, 'technics', true));
+            // Attribuer une image aléatoire depuis le tableau
+            $article->setImage($imageNames[array_rand($imageNames)]);
 
             $manager->persist($article);
 
