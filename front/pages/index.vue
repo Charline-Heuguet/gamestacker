13c3e19908c4598691ebda9073906fe3c2f33a50
Vincent-Antoine <vincentcomparato69@gmail.com>
2024-11-26 11:45:29 +0100

    quelques améliorations de structure HTML (hn)

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="gutter">
-    <div class="title">
-      <h1 class="text-gray-900 dark:text-white">Game Stackers</h1>
-    </div>
+    <header class="title">
+      <!-- H1 enrichi avec des mots-clés pour le SEO -->
+      <h1
+        class="text-gray-900 dark:text-white text-3xl font-bold text-center my-8"
+      >
+        Game Stackers - Actualités, Annonces et Forums sur les Jeux Vidéo
+      </h1>
+    </header>
 
-    <section>
-      <latestArticles />
-      <!-- <categories /> -->
-      <announcement />
-      <LatestForum />
-    </section>
+    <main>
+      <!-- Section des derniers articles -->
+      <section aria-label="Derniers Articles sur les Jeux Vidéo" class="mb-12">
+        <div>
+          <latestArticles />
+        </div>
+      </section>
+
+      <!-- Section des annonces -->
+      <section aria-label="Annonces et Parties de Jeux Vidéo" class="mb-12">
+        <div>
+          <announcement />
+        </div>
+      </section>
+
+      <!-- Section des forums -->
+      <section aria-label="Dernières Discussions du Forum" class="mb-12">
+        <div>
+          <LatestForum />
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
